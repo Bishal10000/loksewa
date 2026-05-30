547c7b2d7647de0f1365f0b116a29263f1208204
Bishal Aryal <bishalaryal975@gmail.com>
2026-05-30 18:26:44 +0545

    fix: create sqlite and migrate

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd /var/www
+touch /var/www/database/database.sqlite
 /usr/local/bin/php artisan config:clear
 /usr/local/bin/php artisan config:cache
+/usr/local/bin/php artisan migrate --force
 /usr/local/bin/php artisan serve --host=0.0.0.0 --port=8000
