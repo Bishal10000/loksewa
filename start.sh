736ce1887c3b7203e7a802dc86de4cfe4e1daac4
Bishal Aryal <bishalaryal975@gmail.com>
2026-06-03 21:58:28 +0545

    chore: switch to PostgreSQL for production

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd /var/www
-touch /var/www/database/database.sqlite
 /usr/local/bin/php artisan config:clear
 /usr/local/bin/php artisan config:cache
-/usr/local/bin/php artisan migrate --force
-/usr/local/bin/php artisan serve --host=0.0.0.0 --port=8000
+/usr/local/bin/php artisan serve --host=0.0.0.0 --port="${PORT:-8000}"
